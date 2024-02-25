6a8b5323fffb74c2862c111f084cc018ac7e09b4
Malu Dee <malud@spotify.com>
2024-02-25 11:14:57 -0500

    separate login page for users

--- rent_wise_nyc/users/urls.py
+++ rent_wise_nyc/users/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home),
     path("logout", views.logout_view),
-    path('registration/login/', views.landlord_login, name='login'),
+    path('registration/landlord_login', views.landlord_login, name='landlord_login'),
+    path('registration/user_login', views.user_login, name='user_login'),
     path('landlord/', views.landlord_home, name='landlord'),
     path('users/', views.user_home, name='user'),
 ]
--- rent_wise_nyc/users/views.py
+++ rent_wise_nyc/users/views.py
@@ -32,14 +32,34 @@ def landlord_login(request):
         if user is not None:
             login(request, user)
             logging.info(f"logged in {user=}")
-            return render(request,"landlords.html")
+            return render(request,"landlord_homepage.html")
         else:
             # Return an error message or render the login page again
             messages.error(request,'Invalid credentials!!')
             logging.info("invalid credentials")
-            return render(request, 'registration/login.html', {'error': 'Invalid credentials!'})
+            return render(request, 'registration/landlord_login.html', {'error': 'Invalid credentials!'})
     else:
-        return render(request,"registration/login.html")
+        return render(request,"registration/landlord_login.html")
+    
+def user_login(request):
+    logging.info("on login page")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        logging.info(f"logging in as {username}")
+        user = authenticate(username=username, password=password)
+        logging.info(f"{user=}")
+        if user is not None:
+            login(request, user)
+            logging.info(f"logged in {user=}")
+            return render(request,"user_homepage.html")
+        else:
+            # Return an error message or render the login page again
+            messages.error(request,'Invalid credentials!!')
+            logging.info("invalid credentials")
+            return render(request, 'registration/user_login.html', {'error': 'Invalid credentials!'})
+    else:
+        return render(request,"registration/user_login.html")
 
 def home(request):
     return render(request,"home.html")
