3e20fad90513c84b170a82307be028b78f55e479
Malu Dee <malud@spotify.com>
2024-02-25 11:28:39 -0500

    two different auth

--- rent_wise_nyc/users/views.py
+++ rent_wise_nyc/users/views.py
@@ -21,45 +21,40 @@ LOGGING = {
 
 logging.config.dictConfig(LOGGING)
 
-def landlord_login(request):
-    logging.info("on login page")
+def login_process(request, user_type, this_page, destination):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         logging.info(f"logging in as {username}")
         user = authenticate(username=username, password=password)
-        logging.info(f"{user=}")
-        if user is not None:
+        logging.info(f"{user.user_type=}")
+        if user.user_type == user_type:
             login(request, user)
             logging.info(f"logged in {user=}")
-            return render(request,"landlord_homepage.html")
+            return render(request, destination)
         else:
             # Return an error message or render the login page again
             messages.error(request,'Invalid credentials!!')
             logging.info("invalid credentials")
-            return render(request, 'registration/landlord_login.html', {'error': 'Invalid credentials!'})
+            return render(request, this_page, {'error': 'Invalid credentials!'})
     else:
-        return render(request,"registration/landlord_login.html")
+        return render(request, this_page)
+
+def landlord_login(request):
+    return login_process(
+        request, 
+        user_type="landlord", 
+        this_page="registration/landlord_login.html", 
+        destination="landlord_homepage.html"
+    )
     
 def user_login(request):
-    logging.info("on login page")
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        logging.info(f"logging in as {username}")
-        user = authenticate(username=username, password=password)
-        logging.info(f"{user=}")
-        if user is not None:
-            login(request, user)
-            logging.info(f"logged in {user=}")
-            return render(request,"user_homepage.html")
-        else:
-            # Return an error message or render the login page again
-            messages.error(request,'Invalid credentials!!')
-            logging.info("invalid credentials")
-            return render(request, 'registration/user_login.html', {'error': 'Invalid credentials!'})
-    else:
-        return render(request,"registration/user_login.html")
+    return login_process(
+        request, 
+        user_type="user", 
+        this_page="registration/user_login.html", 
+        destination="user_homepage.html"
+    )
 
 def home(request):
     return render(request,"home.html")
