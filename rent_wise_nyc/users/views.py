140d73a42baeb576404e8ba687241e095d94bc58
Yanamadala Sai Aravind <60089478+aru456@users.noreply.github.com>
2024-02-26 19:22:03 -0500

    Added form validations

--- rent_wise_nyc/users/views.py
+++ rent_wise_nyc/users/views.py
@@ -23,20 +23,34 @@ logging.config.dictConfig(LOGGING)
 
 def login_process(request, user_type, this_page, destination):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        logging.info(f"logging in as {username}")
+        username = request.POST.get('username')  # Use .get to avoid KeyError
+        password = request.POST.get('password')
+
+        if not username or not password:
+            messages.error(request, 'Username and password are required!!!!')
+            return render(request, this_page, {'error': 'Username and password are required!'})
+
+        logging.info(f"Logging in as {username}")
         user = authenticate(username=username, password=password)
-        logging.info(f"{user.user_type=}")
-        if user.user_type == user_type:
-            login(request, user)
-            logging.info(f"logged in {user=}")
-            return render(request, destination)
-        else:
-            # Return an error message or render the login page again
-            messages.error(request,'Invalid credentials!!')
-            logging.info("invalid credentials")
+
+        if user is None:
+            messages.error(request, 'Invalid credentials!!')
             return render(request, this_page, {'error': 'Invalid credentials!'})
+
+        logging.info(f"{user.user_type=}")
+        fetch_type = getattr(user, 'user_type', None)
+        if fetch_type == user_type:
+            login(request, user)
+            logging.info(f"Logged in {user=}")
+            return render(request, destination)
+        else :
+            if(fetch_type == 'landlord'):
+                messages.error(request, 'Please provide user credentials to login as User !!')
+                return render(request, this_page, {'error': 'Invalid user type!'})
+            else :
+                messages.error(request, 'Please provide landlord credentials to login as Landlord !!')
+                return render(request, this_page, {'error': 'Invalid user type!'})
+                
     else:
         return render(request, this_page)
 
