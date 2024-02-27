3c437cea8f49cbb5801a89dd31015bf0f4524944
Yanamadala Sai Aravind <60089478+aru456@users.noreply.github.com>
2024-02-27 17:23:29 -0500

    modified path urls for logins

--- rent_wise_nyc/users/urls.py
+++ rent_wise_nyc/users/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home),
     path("logout", views.logout_view),
-    path('registration/landlord_login', views.landlord_login, name='landlord_login'),
-    path('registration/user_login', views.user_login, name='user_login'),
+    path('login/landlord_login', views.landlord_login, name='landlord_login'),
+    path('login/user_login', views.user_login, name='user_login'),
     path('landlord/', views.landlord_home, name='landlord'),
     path('users/', views.user_home, name='user'),
 ]
