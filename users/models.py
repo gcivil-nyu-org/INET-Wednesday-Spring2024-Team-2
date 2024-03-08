8b864f10cba59447ceab23d31ff46ba373b01703
aravind <60089478+aru456@users.noreply.github.com>
2024-03-08 18:57:43 -0500

    added fileds in models.py

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class CustomUser(AbstractUser):
     USER = "user"
@@ -10,3 +9,15 @@ class CustomUser(AbstractUser):
         (LANDLORD, "Landlord"),
     ]
     user_type = models.CharField(max_length=20, choices=USER_TYPES, default=USER)
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    city = models.CharField(max_length=100)
+    verified = models.BooleanField(default=True)
+    s3_doclink = models.CharField(max_length=255, blank=True, null=True)  # Link to S3 document
+
+    def save(self, *args, **kwargs):
+        if self.user_type == CustomUser.USER:
+            self.verified = True
+        else:
+            self.verified = False
+        super().save(*args, **kwargs)
