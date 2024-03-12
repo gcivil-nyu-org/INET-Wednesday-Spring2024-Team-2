38b44f90a06239f6ee536b0aa2ad710ab36475de
SiriErrabelli <148021278+SiriErrabelli@users.noreply.github.com>
2024-03-12 00:41:16 -0400

    fixed build fails

--- users/forms.py
+++ users/forms.py
@@ -43,9 +43,23 @@ class LandlordSignupForm(UserCreationForm):
             "pdf_file",
         )
 
+    # def _init_(self, *args, **kwargs):
+    #     super(LandlordSignupForm, self)._init_(*args, **kwargs)
+    #     self.fields["user_type"].initial = CustomUser.LANDLORD
+    #     self.fields[
+    #         "user_type"
+    #     ].widget = forms.HiddenInput()  # Hide the user_type field
+
     def _init_(self, *args, **kwargs):
         super(LandlordSignupForm, self)._init_(*args, **kwargs)
-        self.fields["user_type"].initial = CustomUser.LANDLORD
-        self.fields[
-            "user_type"
-        ].widget = forms.HiddenInput()  # Hide the user_type field
+        # No need to set user_type initial value here, as it will be set in save.
+
+    def save(self, commit=True):
+        user = super(LandlordSignupForm, self).save(commit=False)
+        user.user_type = CustomUser.LANDLORD  # Set user_type to LANDLORD
+
+        if commit:
+            user.save()
+            self.save_m2m()  # Call save_m2m if there are many-to-many fields that need to be saved.
+
+        return user
--- users/models.py
+++ users/models.py
@@ -16,13 +16,13 @@ class CustomUser(AbstractUser):
     verified = models.BooleanField(default=True)
     s3_doclink = models.CharField(max_length=255, blank=True, null=True)
 
-    user_type = models.CharField(max_length=20, choices=USER_TYPES, default=LANDLORD)
-    city = models.CharField(max_length=100)
-    full_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    verified = models.CharField(max_length=100, default="false")
-    s3_doclink = models.URLField(max_length=255, blank=True, null=True)
+    # user_type = models.CharField(max_length=20, choices=USER_TYPES, default=LANDLORD)
+    # city = models.CharField(max_length=100)
+    # full_name = models.CharField(max_length=100)
+    # phone_number = models.CharField(max_length=100)
+    # email = models.CharField(max_length=100)
+    # verified = models.CharField(max_length=100, default="false")
+    # s3_doclink = models.URLField(max_length=255, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if self.user_type == CustomUser.USER:
