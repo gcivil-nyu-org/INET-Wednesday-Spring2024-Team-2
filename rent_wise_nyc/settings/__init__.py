621b3cc67a718c607fd7ba26d3d63dcfab51271b
Malu Dee <md5024@nyu.edu>
2024-03-12 23:09:02 -0400

    ignore flake error

--- rent_wise_nyc/settings/__init__.py
+++ rent_wise_nyc/settings/__init__.py
@@ -1,9 +1,9 @@
-from .base import *
+from .base import * # noqa: <F403>
 import os
 
 if os.environ.get("ENV_NAME") == "prod":
-    from .prod import *
+    from .prod import * # noqa: <F403>
 elif os.environ.get("ENV_NAME") == "develop":
-    from .develop import *
+    from .develop import * # noqa: <F403>
 else:
-    from .local import *
+    from .local import * # noqa: <F403>
