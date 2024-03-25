350d76a6edd4fbb72107c4eb57d45996b00662a9
Malu Dee <md5024@nyu.edu>
2024-03-25 16:50:03 -0400

    take out email validation for email reset

--- users/forms.py
+++ users/forms.py
@@ -73,23 +73,6 @@ class LandlordSignupForm(UserCreationForm):
         return user
 
 
-# class PasswordResetForm(forms.Form):
-#     email = forms.EmailField(label="Email", max_length=254)
-
-#     def clean_email(self):
-#         email = self.cleaned_data.get("email")
-#         # if not User.objects.filter(email=email).exists():
-#         if not User.objects.filter(email=self.cleaned_data['email']).exists():
-
-#             raise forms.ValidationError(
-#                 """This email does not exist in our records.
-#                                         Please make sure you entered it correctly,
-#                                         or sign up for a new account
-#                                         if you haven't already."""
-#             )
-#         return email
-
-
 class CustomLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
